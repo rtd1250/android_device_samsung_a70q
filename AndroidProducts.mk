4179dfbe1010470a28e3f0d740bcb518a799e214
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    a70q: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_a70q.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a70q-user \
+    lineage_a70q-userdebug \
+    lineage_a70q-eng
--- vendorsetup.sh
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2019 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo lineage_a70q-eng
-add_lunch_combo lineage_a70q-userdebug
