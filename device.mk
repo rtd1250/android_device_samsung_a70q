5e66f3c5f6443003a55eec4e8bf647178023f33c
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    a70q: Adding Audio HAL V5: Direction API
    Bug: 119137468
    Test: With testbed app
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa

--- device.mk
+++ device.mk
@@ -74,12 +74,12 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio@4.0 \
+    android.hardware.audio@5.0 \
     android.hardware.audio.common@4.0 \
     android.hardware.audio.common@4.0-util \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0 \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0 \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl:32 \
     audio.a2dp.default \
     audio.primary.sm6150 \
