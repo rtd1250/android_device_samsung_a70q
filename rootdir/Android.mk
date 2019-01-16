c21de6fcb59e913356ea20e3504b7ea97241d966
Alistair Strachan <astrachan@google.com>
2019-01-16 14:10:47 -0800

    a70q: rootdir: Use first_stage_mount feature
    Change-Id: I9c2a8c140ed64faecee0d66e00033fdffa398a4b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,6 +9,15 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.a70q.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
