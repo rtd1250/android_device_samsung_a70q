6367ff783abe90164b3dd676175dcae599248ecd
Willi Ye <williye97@gmail.com>
2019-09-07 18:11:00 +0200

    props: Remove wrong pref values

--- system_prop.mk
+++ system_prop.mk
@@ -57,8 +57,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.core_ctl_min_cpu=2 \
-    ro.vendor.qti.core_ctl_max_cpu=4 \
     vendor.iop.enable_prefetch_ofr=0 \
     vendor.iop.enable_uxe=1 \
     vendor.perf.gestureflingboost.enable=true \
