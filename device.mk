aa3b585666e85f3e312a39c088d6e734c8bd2481
Willi Ye <williye97@gmail.com>
2019-09-09 17:00:23 +0200

    ril: Kang radio interface from https://github.com/phhusson/vendor_interfaces/tree/pie/vendor/samsung/hardware/radio/1.2

--- device.mk
+++ device.mk
@@ -314,7 +314,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
+    vendor.samsung.hardware.radio@1.2.vendor \
     android.hardware.radio.config@1.0 \
     libjson \
     librmnetctl \
