6b645f404c7f6dc4e6bd6d3506cf71ea88636c36
Willi Ye <williye97@gmail.com>
2019-10-06 20:51:28 +0200

    ril: Add symbols which might be used in the future
    Change-Id: Ibeb86a0e33810c02b0adbf84e4849b647d8186ee

--- ril/Android.mk
+++ ril/Android.mk
@@ -5,4 +5,7 @@ LOCAL_SRC_FILES := ril.c
 LOCAL_MODULE := libshims_ril
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
+
+#LOCAL_SHARED_LIBRARIES := liblog libcutils
+
 include $(BUILD_SHARED_LIBRARY)
--- ril/ril.c
+++ ril/ril.c
@@ -1,5 +1,71 @@
+//#include <cutils/log.h>
 #include <stdint.h>
+//#include <sys/statfs.h>
 
 int64_t _ZN14TrafficControl15connectToServeeEb(uint8_t value __unused) {
     return -1;
 }
+
+/*int64_t RIL_GetSettingValue(char *key, char *value) {
+    ALOGE("%s: blaa key %s value %s", __func__, key, value);
+    return 0;
+}
+
+int64_t RIL_GetFd(char *arg0, int arg1, int arg2) {
+    ALOGE("%s: blaa %s %d %d", __func__, arg0, arg1, arg2);
+    return 0;
+}
+
+int64_t RIL_Execute(char *arg0) {
+    ALOGE("%s: blaa %s", __func__, arg0);
+    return 0;
+}
+
+int64_t RIL_Unlink(char *arg0) {
+    ALOGE("%s: blaa %s", __func__, arg0);
+    return 0;
+}
+
+int64_t RIL_Mkdir(char *path, int permission) {
+    ALOGE("%s: blaa %s %d", __func__, path, permission);
+    return 0;
+}
+
+int64_t RIL_Chmod(char *path, int permission) {
+    ALOGE("%s: blaa %s %d", __func__, path, permission);
+    return 0;
+}
+
+int64_t RIL_Chown(char *path, int user, int group) {
+    ALOGE("%s: blaa %s %d %d", __func__, path, user, group);
+    return 0;
+}
+
+int64_t RIL_Stat(char *path) {
+    ALOGE("%s: blaa %s", __func__, path);
+    return 0;
+}
+
+int64_t RIL_Rename(char *path, char *new_path) {
+    ALOGE("%s: blaa %s %s", __func__, path, new_path);
+    return 0;
+}
+
+int64_t RIL_StatFs(char *path, struct statfs *statfs __unused) {
+    ALOGE("%s: blaa %s", __func__, path);
+    return 0;
+}
+
+int64_t RIL_IoCtl(int arg0, int64_t arg1, int *arg2) {
+    ALOGE("%s: blaa %d %ld %d", __func__, arg0, arg1, *arg2);
+    return 0;
+}
+
+int64_t RIL_GetFdWithSetAttr(char *arg0, int arg1, int arg2, int *arg3) {
+    ALOGE("%s: blaa %s %d %d %d", __func__, arg0, arg1, arg2, *arg3);
+    return 0;
+}
+
+void RIL_LoadApnProfFromDB(char *arg0, void *arg1 __unused) {
+    ALOGE("%s: blaa %s", __func__, arg0);
+}*/
