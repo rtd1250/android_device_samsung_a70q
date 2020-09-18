56d2d9960e0cdff209b0cfaf10c5f0ce82ba0316
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    a70q: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -202,7 +202,11 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor
+    android.hidl.base@1.0.vendor \
+	libhidltransport \
+	libhidltransport.vendor \
+	libhwbinder \
+	libhwbinder.vendor
 
 # IPACM
 PRODUCT_PACKAGES += \
