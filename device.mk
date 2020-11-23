78bb30c8cde902b9f29697cf726a0fd436c630bb
Ataberk Özen <ataberkozen123@gmail.com>
2020-11-23 06:13:48 +0000

    Revert "a70q: build camera.device impls from source"
    This reverts commit e829c02fa78fc43f069c34ee36c1662e472bf723.
    
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -134,13 +134,6 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    camera.device@3.3-impl \
-    camera.device@3.4-impl \
-    camera.device@3.5-impl
-
-PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5-legacy
 
 # Display/Graphics
