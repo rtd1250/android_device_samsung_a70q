8967d1c864a138ed23fb69de9247c24a4a5439a2
Ataberk Özen <ataberkozen123@gmail.com>
2020-12-26 13:47:50 +0000

    a70q: use custom radio hidl from hardware/samsung
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -330,7 +330,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.3-radio-service.a70q \
+    android.hardware.radio@1.3-radio-service.samsung \
     vendor.samsung.hardware.radio@1.2.vendor \
     android.hardware.radio.config@1.0 \
     libjson \
