52d6559bf11ce37d20d4939e9d62731f5bee8b98
Ataberk Özen <ataberkozen123@gmail.com>
2020-12-27 21:17:00 +0000

    a70q: use qti thermal 2.0
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -366,9 +366,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sm6150
+    android.hardware.thermal@2.0-service.qti
 
 # TextClassifier
 PRODUCT_PACKAGES += \
