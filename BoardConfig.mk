000cf5b1c1772769a9fb3122be201e4c726b4226
Ataberk Özen <ataberkozen123@gmail.com>
2021-02-06 19:00:57 +0300

    a70q: enable Wi-Fi STA/AP concurrency
    * This flag exposes wlan1 interface.
    
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -274,6 +274,7 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_AP  := "ap"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # Inherit from the proprietary version
 -include vendor/samsung/a70q/BoardConfigVendor.mk
