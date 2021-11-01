d6aeaa5ddf5877cd9d0c6f506c257a3119859ae2
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-11-01 16:10:06 +0100

    a70q: Import vibrator HIDL from hardware/samsung
    Change-Id: Ifd9eb333072438934e76cf98bc6c2fdc56105458

--- device.mk
+++ device.mk
@@ -425,8 +425,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.3-service.a70q
 
 # VNDK
 PRODUCT_COPY_FILES += \
