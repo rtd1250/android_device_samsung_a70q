2883e7d6dfd5b95340fb2e8dae8e1f2b901c1dad
Sal Savage <salsavage@google.com>
2022-02-04 09:41:57 -0800

    a70q: Enable bluetooth profiles with product.prop
    Bug: 217448211
    Test: make -j
    Change-Id: Idd7e52b7822915ed61299f1c0b29bf87ca0339fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,6 +188,7 @@ TARGET_USES_MKE2FS := true
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
