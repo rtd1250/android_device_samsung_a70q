b6506c5165dea25ed4311c96b5bcf409c4ac60e1
rtd1250 <47224354+rtd1250@users.noreply.github.com>
2022-03-16 21:15:37 +0100

    Remove custom ril impl Possibly outdated, switching to Samsung's rild.

--- ril/libtrafficcontrol/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := traffic_control.c
-LOCAL_MODULE := libtrafficcontrol-spoofer
-LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
--- ril/libtrafficcontrol/traffic_control.c
+++
@@ -1,5 +0,0 @@
-#include <stdint.h>
-
-int64_t _ZN14TrafficControl15connectToServeeEb(uint8_t value __unused) {
-    return -1;
-}
