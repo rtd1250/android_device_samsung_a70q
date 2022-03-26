fd9eed2ea33fd3745120e85b6c5f75cb9bb9b081
rtd1250 <47224354+rtd1250@users.noreply.github.com>
2022-03-26 17:52:59 +0100

    Update USB HAL to 1.3, get rid of prebuilt 1.1

--- device.mk
+++ device.mk
@@ -419,8 +419,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0.vendor \
-    android.hardware.usb@1.1.vendor
+    android.hardware.usb@1.3-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
