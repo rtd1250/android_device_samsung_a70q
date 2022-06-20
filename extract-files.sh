7da680854be1d2ceeff3cf4674afbd96d58d2cef
LuK1337 <priv.luk@gmail.com>
2022-06-20 12:27:27 +0200

    a70q: Pass call state to audio hal and patch RIL
    Simon1511: libsec-ril sets a prop (ril.dds.call.slotid) when in call with Sim1 or Sim2. Patch libsec-ril to make that prop vendor lable-able (vendor.calls.slotid). Then use that prop to check which Sim is currently in a call (0 for Sim1 and 1 for Sim2) and pass the arguments to audio HAL depending on that.
    
    Note for later: Check whether passing [g_]call_state is really needed
    
    Change-Id: I5a0dfd3b100287a2cf1ec3c156292487dc4da3d5

--- extract-files.sh
+++ extract-files.sh
@@ -36,6 +36,12 @@ source "${HELPER}"
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/libsec-ril.so)
+            sed -i 's/ril.dds.call.slotid/vendor.calls.slotid/g' "${2}"
+            ;;
+        vendor/lib64/libsec-ril-dsds.so)
+            sed -i 's/ril.dds.call.slotid/vendor.calls.slotid/g' "${2}"
+            ;;
         vendor/lib64/hw/android.hardware.health@2.0-impl-2.1-samsung.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             ;;
