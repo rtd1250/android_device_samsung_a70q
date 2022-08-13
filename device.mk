dc28598b872799149fd85a5028fc000b205ddc87
rtd1250 <47224354+rtd1250@users.noreply.github.com>
2022-08-13 18:06:39 +0200

    Build qti display mapper@1.1

--- device.mk
+++ device.mk
@@ -149,6 +149,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer@1.0.vendor \
     vendor.qti.hardware.display.composer@2.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \
