93b05bb64759c0ed710a8b4325f6a7f74d035ec2
rtd1250 <47224354+rtd1250@users.noreply.github.com>
2022-09-24 19:38:18 +0200

    Revert to radio@1.4 This was a terrible mistake.

--- device.mk
+++ device.mk
@@ -368,7 +368,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5.vendor \
+    android.hardware.radio@1.4.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     libjson \
