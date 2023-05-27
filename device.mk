82f537941288b74dc4ccbf3b39bfd23e95318d67
Andrzej Szafran <rtd1250@gmail.com>
2023-05-27 17:28:39 +0200

    a70q: Build necessary camera packages

--- device.mk
+++ device.mk
@@ -131,7 +131,14 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-legacy
+    android.frameworks.cameraservice.common@2.0.vendor \
+    android.frameworks.cameraservice.device@2.0.vendor \
+    android.frameworks.cameraservice.service@2.1.vendor \
+    android.frameworks.cameraservice.service@2.0.vendor \
+    android.hardware.camera.provider@2.6-service \
+    android.hardware.camera.provider@2.6.vendor \
+    android.hardware.camera.device@3.6.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Display/Graphics
 PRODUCT_PACKAGES += \
