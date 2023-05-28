16eb853374d323471632b054917ae74e45b38190
Simon <46072379+Simon1511@users.noreply.github.com>
2023-05-28 12:50:10 +0300

    Import touch HAL from hardware/samsung and add singleton support

--- device.mk
+++ device.mk
@@ -418,6 +418,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
 
+# Touch features
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.a70q
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
