4602324ff1d2a0f73c790f1d532db346a15fe950
Andrzej Szafran <rtd1250@gmail.com>
2023-10-22 19:04:42 +0200

    a70q: Build libprotobuf-cpp-lite-3.9.1-vendorcompat * From hardware/lineage/compat

--- device.mk
+++ device.mk
@@ -317,7 +317,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
 # Preopt SystemUI
- PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
+
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # QCOM
 PRODUCT_COPY_FILES += \
