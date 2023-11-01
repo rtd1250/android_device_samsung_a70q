087e1045cbb0891b9878419991332a0cc1515ff3
Andrzej Szafran <rtd1250@gmail.com>
2023-11-01 19:10:07 +0100

    a70q: Add Gatekeeper HAL to libcrypto fixups * Looks like it also breaks when linked against new libcrypto

--- extract-files.sh
+++ extract-files.sh
@@ -45,7 +45,7 @@ function blob_fixup() {
         vendor/lib64/hw/android.hardware.health@2.0-impl-2.1-samsung.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             ;;
-        vendor/lib64/libskeymaster4device.so)
+        vendor/lib64/libskeymaster4device.so | vendor/lib64/hw/gatekeeper.mdfpp.so)
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
     esac
